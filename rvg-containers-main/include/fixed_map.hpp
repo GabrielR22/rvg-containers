113f9f9827963a8cf42590112be3c942c5f59197
Gabriel Romanzini <gabrielvuelmar@gmail.com>
2023-06-09 01:40:27 -0300

    fixed_map added operator [] + last found and some other updates

--- rvg-containers-main/include/fixed_map.hpp
+++ rvg-containers-main/include/fixed_map.hpp
@@ -1,27 +1,27 @@
 #pragma once
 
 /*
-	MIT License
+    MIT License
 
-	Copyright(c) 2023 Gabriel Romanzini
+    Copyright(c) 2023 Gabriel Romanzini
 
-	Permission is hereby granted, free of charge, to any person obtaining a copy
-	of this softwareand associated documentation files(the "Software"), to deal
-	in the Software without restriction, including without limitation the rights
-	to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
-	copies of the Software, and to permit persons to whom the Software is
-	furnished to do so, subject to the following conditions :
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this softwareand associated documentation files(the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions :
 
-	The above copyright noticeand this permission notice shall be included in all
-	copies or substantial portions of the Software.
+    The above copyright noticeand this permission notice shall be included in all
+    copies or substantial portions of the Software.
 
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
-	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-	SOFTWARE.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
 */
 
 //std::pair - std::initializer_list
@@ -32,128 +32,151 @@
 
 namespace rvg
 {
-	template <typename Key, typename Value, size_t Size>
-	class fixed_map
-	{
-	public:
+    template <typename Key, typename Value, size_t Size>
+    class fixed_map
+    {
+    public:
+        using Type = std::pair<Key, Value>;
+        using Slot = std::pair<Type, bool>;
 
-		using Type = std::pair<Key, Value>;
-		using Slot = std::pair<Type, bool>;
+        constexpr fixed_map() = default;
 
-		constexpr fixed_map() = default;
+        constexpr fixed_map(std::initializer_list<Type> list)
+        {
+            //static_assert(list.size());
 
-		constexpr fixed_map(std::initializer_list<Type> list)
-		{
-			//assert(list.size() && list.size() < Size);
+            //rvg -> improvement place?
+            size_t index = 0;
+            for (auto it = list.begin(), end = list.end(); it < end; ++it, ++index)
+            {
+                m_Data[index].first = *it;
+                m_mark_used(&m_Data[index]);
+            }
+        };
 
-			//rvg -> improvement place?
-			size_t index = 0;
-			for (auto it = list.begin(), end = list.end(); it < end; ++it, ++index)
-			{
-				m_Data[index].first = *it;
-				m_mark_used(&m_Data[index]);
-			}
-		};
+        //! Main find function, if we get a result we return the address, else we return nulltpr.
+        constexpr Value* find(const Key& k_) { if (auto* ls = m_find_slot(k_)) return &ls-> first.second; else return nullptr; };
+        constexpr const Value* find(const Key& k_) const { if (auto* ls = m_find_slot(k_)) return &ls-> first.second; else return nullptr; };
 
-		//! Main find function, if we get a result we return the address, else we return nulltpr.
-		constexpr Value* find(const Key& k_) { if (auto* ls = m_find_slot(k_)) return &ls->first.second; else return nullptr; };
-		constexpr const Value* find(const Key& k_) const { if (auto* ls = m_find_slot(k_)) return &ls->first.second; else return nullptr; };
+        //! Gets the element in this position. UB if the element doesn't exist.
+        constexpr Value& at(const Key& k_) { return m_find_slot(k_, true)->first.second; }
+        constexpr const Value& at(const Key& k_) const { return m_find_slot(k_, true)->first.second; }
 
-		//! Gets the element in this position. UB if the element doesn't exist.
-		constexpr Value& at(const Key& k_) { return m_find_slot(k_, true)->first; }
-		constexpr const Value& at(const Key& k_) const { return m_find_slot(k_, true)->first; }
+        constexpr Value& operator [](const Key& k_)
+        {
+            if (auto* slot = m_find_slot(k_))
+                return slot->first.second;
 
-		//! Get last element.
-		constexpr Value& get_last_found() {
-			if (m_LastFind.has_value()) return m_LastFind.value().get().second;
-			else throw std::out_of_range("you must use find or contains before calling get_last_found!");
-		};
+            if (auto* slot = m_get_first_empty())
+            {
+                m_mark_used(slot);
+                return slot->first.second;
+            }
 
-		//! Get last element (CONST).
-		constexpr const Value& get_last_found() const {
-			if (m_LastFind.has_value()) return m_LastFind.value().get().second;
-			else throw std::out_of_range("you must use find or contains before calling get_last_found!");
-		};
+            throw std::exception("failed to find empty slot in map!");
+        };
 
-		//! Returns if the map contains this key.
-		constexpr bool contains(const Key& k_) const
-		{
-			if (const Slot* sl = m_find_slot(k_))
-			{
-				m_set_last(sl->first);
-				return true;
-			}
+        //! Get last element.
+        constexpr Value& get_last_found()
+        {
+            if (m_LastFind.has_value()) return m_LastFind.value().get().second;
+            else throw std::out_of_range("you must use find or contains before calling get_last_found!");
+        };
 
-			m_clear_last();
-			return false;
-		}
+        //! Get last element (CONST).
+        constexpr const Value& get_last_found() const
+        {
+            if (m_LastFind.has_value()) return m_LastFind.value().get().second;
+            else throw std::out_of_range("you must use find or contains before calling get_last_found!");
+        };
 
-		//! Request a new place in the map, if we find an empty slot, we return the addres, else nullptr.
-		[[nodiscard]] constexpr Type* get_free_slot()
-		{
-			if (Slot* slot = m_get_first_empty())
-			{
-				m_mark_used(slot);
-				return &slot->first;
-			}
+        //! Returns if the map contains this key.
+        constexpr bool contains(const Key& k_) const
+        {
+            if (const Slot* sl = m_find_slot(k_))
+            {
+                m_set_last(sl->first);
+                return true;
+            }
 
-			return nullptr;
-		}
+            m_clear_last();
+            return false;
+        }
 
-		constexpr bool erase(const Key& k_) { if (auto* sl = m_find_slot(k_)) { m_mark_unused(sl); return true; } else return false; }
+        //! Request a new place in the map, if we find an empty slot, we return the addres, else nullptr.
+        [[nodiscard]] constexpr Type* get_free_slot()
+        {
+            if (Slot* slot = m_get_first_empty())
+            {
+                m_mark_used(slot);
+                return &slot->first;
+            }
 
-	private:
+            return nullptr;
+        }
 
-		[[nodiscard]] constexpr Slot* m_find_slot(const Key& k_, const bool exception = false)
-		{
-			for (auto& sl : m_Data)
-			{
-				if (sl.second == true && sl.first.first == k_)
-					return &sl;
-			}
+        constexpr bool erase(const Key& k_)
+        {
+            if (auto* sl = m_find_slot(k_))
+            {
+                m_mark_unused(sl);
+                return true;
+            }
+            else return false;
+        }
 
-			if (exception) throw std::out_of_range("failed to get element in fixed_map");
+    private:
+        [[nodiscard]] constexpr Slot* m_find_slot(const Key& k_, const bool exception = false)
+        {
+            for (auto& sl : m_Data)
+            {
+                if (sl.second == true && sl.first.first == k_)
+                    return &sl;
+            }
 
-			return nullptr;
-		}
-		[[nodiscard]] constexpr const Slot* m_find_slot(const Key& k_) const
-		{
-			for (auto& sl : m_Data)
-			{
-				if (sl.second == true && sl.first.first == k_)
-				{
-					m_set_last(sl.first);
-					return &sl;
-				}
-			}
+            if (exception) throw std::out_of_range("failed to get element in fixed_map");
 
-			m_clear_last();
-			return nullptr;
-		}
+            return nullptr;
+        }
 
-		[[nodiscard]] constexpr Slot* m_get_first_empty()
-		{
-			for (auto& sl : m_Data)
-			{
-				if (sl.second == false)
-				{
-					m_set_last(sl.first);
-					return &sl;
-				}
-			}
+        [[nodiscard]] constexpr const Slot* m_find_slot(const Key& k_) const
+        {
+            for (auto& sl : m_Data)
+            {
+                if (sl.second == true && sl.first.first == k_)
+                {
+                    m_set_last(sl.first);
+                    return &sl;
+                }
+            }
 
-			return nullptr;
-		}
+            m_clear_last();
+            return nullptr;
+        }
 
-		constexpr void m_clear_last() const { m_LastFind.reset(); };
-		constexpr void m_set_last(const Type& el_) const { m_LastFind = const_cast<Type&>(el_); };
+        [[nodiscard]] constexpr Slot* m_get_first_empty()
+        {
+            for (auto& sl : m_Data)
+            {
+                if (sl.second == false)
+                {
+                    m_set_last(sl.first);
+                    return &sl;
+                }
+            }
 
-		constexpr static void m_mark_used(Slot* sl_) { (*sl_).second = true; }
-		constexpr static void m_mark_unused(Slot* sl_) { (*sl_).second = false; }
+            return nullptr;
+        }
 
-	private:
-		std::array<Slot, Size> m_Data{};
+        constexpr void m_clear_last() const { m_LastFind.reset(); };
+        constexpr void m_set_last(const Type& el_) const { m_LastFind = const_cast<Type&>(el_); };
 
-		mutable std::optional<std::reference_wrapper<Type>> m_LastFind;
-	};
+        constexpr static void m_mark_used(Slot* sl_) { (*sl_).second = true; }
+        constexpr static void m_mark_unused(Slot* sl_) { (*sl_).second = false; }
+
+    private:
+        std::array<Slot, Size> m_Data{};
+
+        mutable std::optional<std::reference_wrapper<Type>> m_LastFind;
+    };
 }
